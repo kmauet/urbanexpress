726f5fa06877e782db933f6f49c0672391991fd2
Ousmane Kebe <kebe.4@osu.edu>
2017-02-26 21:06:43 -0500

    email processing stuff

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -4,36 +4,50 @@ class EmailProcessor
   end
 
   def process
-    # all of your application-specific code here - creating models,
-    # processing reports, etc
+    # quote request subject Quote Request (#32355)
+    quote_id = get_quote_id_from_subject(@email.subject)
+    @from_email = @email.from[:email]
+    user = User.find_by(email: @from_email)
+    customer = Customer.find_by(email: @from_email)
 
-    # here's an example of model creation
-    #quote_id = get_quote_id_from_subject(@email.subject)
-    quote_id = Quote.last.id
     if quote_id.blank?
       #check_header_for_reply_to
-      quote = Quote.create(info: @email.body)
+      create_message_from_email(@email, nil, user, customer)
     else
+      #only trust quote_id if email comming from original customer or urbanexpress user
       quote = Quote.find(quote_id)
+      quote_email = quote.try(:email) || ""
+      if quote_email == @email.from[:email] or not(user.blank?)
+        create_message_from_email(@email, quote.id, user, customer) 
+      end
     end
+    
      
-    if not(quote.blank?) #and quote.email == @email.from
-      Message.create!(
-        body: @email.body,
-        quote_id: quote.id,
-        to: @email.to,
-        from: @email.from,
-        cc: @email.cc,
-        subject: @email.subject,
-        raw_text: @email.raw_text,
-        raw_html: @email.raw_html,
-        raw_body: @email.raw_body,
-        attachments: @email.attachments,
-        headers: @email.headers,
-        raw_headers: @email.raw_headers
-      )
-    else
-      random_message = Message.create(body: @email.body)
-    end
+    
   end
+
+  private
+    def get_quote_id_from_subject(subject)
+      quote_id = subject.match(/\(#([1-9]+)\)/)[1]
+    end
+
+    def create_message_from_email(email, quote_id, user, customer)
+      Message.create!(
+        body: email.body,
+        quote_id: quote_id,
+        to: email.to,
+        from: email.from,
+        cc: email.cc,
+        subject: email.subject,
+        raw_text: email.raw_text,
+        raw_html: email.raw_html,
+        raw_body: email.raw_body,
+        attachments: email.attachments,
+        headers: email.headers,
+        raw_headers: email.raw_headers,
+        user_id: user.try(:id),
+        customer_id: customer.try(:id)
+      )
+    end
 end
+
