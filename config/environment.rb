fc82aad4494e9b1bc7844edff2685a86769d9a09
Ousmane Kebe <kebe.4@osu.edu>
2017-03-02 00:23:18 -0500

    config stuff

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'ousmane.kebe@gmail.com',
+  :user_name => 'app63946855@heroku.com',
   :password => '6qiwjufs4126',
   :domain => 'urbanexpress.zesasoccer.com',
   :address => 'smtp.sendgrid.net',
