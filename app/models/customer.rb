3bf7197bfef1d5333575c23d31a62f82d63e15df
Ousmane Kebe <kebe.4@osu.edu>
2017-03-19 19:21:05 -0400

    make has_invoices_due? boolean

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -46,7 +46,7 @@ class Customer < ApplicationRecord
   end
   
   def has_invoices_due?
-    invoices.where(payed: nil)
+    invoices.where(payed: nil).count > 0 
   end
 
   # Make password not be required for customers that are being created by administrators
