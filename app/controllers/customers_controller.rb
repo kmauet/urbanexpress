317970224764cd87f96e91de23d3147dec590c9a
Ousmane Kebe <kebe.4@osu.edu>
2017-04-08 13:19:35 -0400

    new registration captcha

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -47,7 +47,7 @@ class CustomersController < ApplicationController
     @customer = Customer.new(customer_params)
 
     respond_to do |format|
-      if @customer.save
+      if verify_recaptcha(model: @customer) and @customer.save
         format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
         format.json { render :show, status: :created, location: @customer }
       else
